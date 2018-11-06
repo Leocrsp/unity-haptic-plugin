82c12f11ea70e8989e584d5c1a2785bba43db44f
NearLab-PC\Leocrsp <lcresp2@uic.edu>
2018-11-06 11:38:41 +0100

    Adding first header and source file

---
+++ Unity Plugin/header.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef PLUGIN_HEADER
+#define PLUGIN_HEADER
+
+#include <iostream>
+#include "HD/hd.h"
+#include "HL/hl.h"
+#include "QHHeadersGLUT.h"
+#include "QHHeadersWin32.h"
+#include "HDU/hdu.h"
+#include "HLU/hlu.h"
+#include "GL/glut.h"
+
+#endif
