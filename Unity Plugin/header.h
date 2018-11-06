ee8b2cdd157b8b6f66912a725e9c68c25a3d68f2
NearLab-PC\Leocrsp <lcresp2@uic.edu>
2018-11-06 12:33:31 +0100

    no message

--- Unity Plugin/header.h
+++ Unity Plugin/header.h
@@ -2,13 +2,27 @@
 #ifndef PLUGIN_HEADER
 #define PLUGIN_HEADER
 
+#include <Windows.h>
 #include <iostream>
-#include "HD/hd.h"
-#include "HL/hl.h"
-#include "QHHeadersGLUT.h"
-#include "QHHeadersWin32.h"
-#include "HDU/hdu.h"
-#include "HLU/hlu.h"
-#include "GL/glut.h"
+#include <HD/hd.h>
+#include <HD/hdExport.h>
+#include <HL/hl.h>
+#include <QHHeadersGLUT.h>
+#include <QHHeadersWin32.h>
+#include <HDU/hdu.h>
+#include <HLU/hlu.h>
+#include <GL/glut.h>
+
+#if define DLL_EXPORT
+#define LEO_API __declspec(dllexport)
+#else
+#define LEO_API __declspec(dllexport)
+#endif
+
+extern "C" {
+
+	LEO_API void init();
+
+};
 
 #endif
--- Unity Plugin/main.cpp
+++ Unity Plugin/main.cpp
@@ -1,1 +1,17 @@
 #include "header.h"
+
+LEO_API void init() {
+
+	HHD hHD = hdInitDevice(HD_DEFAULT_DEVICE);
+	//hdEnable(HD_FORCE_OUTPUT);
+	if (HD_DEVICE_ERROR(hdGetError())) {
+
+		exit(-1);
+
+	}
+
+	HHLRC hHLRC = hlCreateContext(hHD);
+	hlMakeCurrent(hHLRC);
+
+
+}
